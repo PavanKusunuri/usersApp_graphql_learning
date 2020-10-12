f18809b96b35d9aec4231ac0f6d7a3fcfd9cb2f5
Pavan <pavantejakusunuri@gmail.com>
2020-10-12 20:37:26 +0530

    Added RootQuery in Project

--- schema/schema.js
+++ schema/schema.js
@@ -13,3 +13,19 @@ const UserType = new GraphQLObjectType({
         age: { type: GraphQLInt }
     }
 });
+
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: {
+                id: { type: GraphQLString }
+            },
+            resolve(parentValue, args) {
+
+            }
+        }
+    }
+})
